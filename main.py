680fd9047e75fe363ee0b6707cabc387f10450f9
Aastha Sharma <121185701+AasthaSharma272@users.noreply.github.com>
2023-04-02 21:58:29 -0400

    Changed Main, download the file.pkl too
    The pickle library saves an object in the file, which you can open again quickly and run the program

--- main.py
+++ main.py
@@ -15,11 +15,24 @@ import main_functions
 from screen_select_gui import LogInScreen
 from PyQt6.QtWidgets import QApplication
 import sys
+import pickle
 
 if __name__ == '__main__':
-    graph = Graph()
-    main_functions.read_csv_and_create_data(graph, 'data/tmdb_5000_movies1.csv', 'data/tmdb_5000_credits.csv')
+    # Below runs the program with 4803 movies and takes the graph saved in the saved_state file
+    with open('file.pkl', 'rb') as file:
+        # Call load method to deserialze
+        graph = pickle.load(file)
     app = QApplication(sys.argv)
     window = LogInScreen(graph)
-    window.show()  # To show the gui window the above code is very static to a window open
+    window.show()
     sys.exit(app.exec())
+
+    # To check if the graph loads properly, uncomment the bottom code, which creates a new graph and loads 400 movies
+    # please comment the above code
+    # The call to load 400 movies will take approx 2 mins!
+    # graph_small = Graph()
+    # main_functions.read_csv_and_create_data(graph_small, 'data/tmdb_5000_movies1.csv', 'data/tmdb_5000_credits.csv')
+    # app = QApplication(sys.argv)
+    # window = LogInScreen(graph_small)
+    # window.show()  # To show the gui window the above code is very static to a window open
+    # sys.exit(app.exec())
